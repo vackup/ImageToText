ea8d223b5244b2f19f494649fdfa700b0a924d62
Hernan Zaldivar <hzaldivar79@hotmail.com>
2019-07-23 17:01:45 -0300

    First version. Takes an image, load it, resize it and display it in grayscale text string

--- ImageToText/Form1.Designer.cs
+++ ImageToText/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.txtImageText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImageToText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Location = new System.Drawing.Point(76, 12);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.Size = new System.Drawing.Size(503, 20);
+            this.txtImagePath.TabIndex = 0;
+            // 
+            // txtImageText
+            // 
+            this.txtImageText.Location = new System.Drawing.Point(12, 38);
+            this.txtImageText.Multiline = true;
+            this.txtImageText.Name = "txtImageText";
+            this.txtImageText.Size = new System.Drawing.Size(1217, 500);
+            this.txtImageText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image Path";
+            // 
+            // btnImageToText
+            // 
+            this.btnImageToText.Location = new System.Drawing.Point(586, 8);
+            this.btnImageToText.Name = "btnImageToText";
+            this.btnImageToText.Size = new System.Drawing.Size(100, 23);
+            this.btnImageToText.TabIndex = 3;
+            this.btnImageToText.Text = "Image To Text";
+            this.btnImageToText.UseVisualStyleBackColor = true;
+            this.btnImageToText.Click += new System.EventHandler(this.btnImageToText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1241, 550);
+            this.Controls.Add(this.btnImageToText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtImageText);
+            this.Controls.Add(this.txtImagePath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.TextBox txtImageText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImageToText;
     }
 }
 
--- ImageToText/Form1.cs
+++ ImageToText/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,65 @@ namespace ImageToText
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnImageToText_Click(object sender, EventArgs e)
+        {
+            var image = Image.FromFile(this.txtImagePath.Text);
+
+            var bitmap = ResizeImage(image, 36, 36);
+
+            for (int y = 0; y < bitmap.Height; y++) //for each row
+            {
+                for (int x = 0; x < bitmap.Width; x++) //for each column
+                {
+                    // Get the color of a pixel within myBitmap.
+                    Color cl = bitmap.GetPixel(x, y);
+
+                    int greyValue = (int)((cl.R * 0.3) + (cl.G * 0.59) + (cl.B * 0.11));
+
+                    this.txtImageText.AppendText(greyValue.ToString().PadLeft(3, '0'));
+                }
+
+                this.txtImageText.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.txtImagePath.Text = @"";
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
     }
 }
